baa6e74e7913000c364ae4c9573b55abd57aa357
Simon Toens <simontoens@gmail.com>
2013-12-29 08:12:29 -0800

    Add textview to replace input textfield.

--- SimpleExpressionEvaluator/MainViewController.h
+++ SimpleExpressionEvaluator/MainViewController.h
@@ -8,6 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MainViewController : UIViewController <UITextFieldDelegate>
+@interface MainViewController : UIViewController
 
 @end
--- SimpleExpressionEvaluator/MainViewController.m
+++ SimpleExpressionEvaluator/MainViewController.m
@@ -10,25 +10,33 @@
 #import "MainViewController.h"
 
 @interface MainViewController()
-@property (nonatomic, strong) IBOutlet UITextField *textField;
-@property (nonatomic, strong) IBOutlet UILabel *resultLabel;
-@property (nonatomic, strong) IBOutlet UITextView *astTextView;
+@property (nonatomic, strong) IBOutlet UITextView *inputTextView;
+@property (nonatomic, strong) IBOutlet UITextField *outputTextView;
 @end
 
 @implementation MainViewController
 
-- (void)viewDidLoad
+- (IBAction)onEval:(id)sender
 {
-    [super viewDidLoad];
-    self.textField.delegate = self;
+    NSString *allText = self.inputTextView.text;
+    NSUInteger currentPosition = self.inputTextView.selectedRange.location;
+    NSRange searchRange = NSMakeRange(0, currentPosition);
+    NSRange resultRange = [allText rangeOfString:@"\n" options:NSBackwardsSearch range:searchRange];
+    NSString *exprToEval = [allText substringWithRange:searchRange];
+    if (resultRange.location != NSNotFound)
+    {
+        NSRange exprRange = NSMakeRange(resultRange.location, currentPosition - resultRange.location);
+        exprToEval = [allText substringWithRange:exprRange];
+    }
+    NSLog(@"EXPR %@", exprToEval);
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    ExpressionEvaluator *eval = [[ExpressionEvaluator alloc] init];
-    NSInteger result = [eval evaluate:self.textField.text];
-    self.resultLabel.text = [NSString stringWithFormat:@"%i", result];
-    self.astTextView.text = eval.prefix;
+//    ExpressionEvaluator *eval = [[ExpressionEvaluator alloc] init];
+//    NSInteger result = [eval evaluate:self.textField.text];
+//    self.resultLabel.text = [NSString stringWithFormat:@"%i", result];
+//    self.astTextView.text = eval.prefix;
     return YES;
 }
 
